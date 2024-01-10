158f7b6196341bf95c3705ac6b0b5a336066533a
cxq <1647715359@qq.com>
2024-01-11 00:19:46 +0800

    update

--- download.py
+++ download.py
@@ -5,16 +5,13 @@
 
 import os
 
-import torch
-from modelscope import snapshot_download, AutoModel, AutoTokenizer
+from modelscope import snapshot_download
 
 
 def download():
-    model_dir = snapshot_download('Shanghai_AI_Laboratory/internlm-chat-7b', cache_dir='/root/data/model',
-                                  revision='v1.0.3')
-    # 设置环境变量
+    # create dir
+    os.makedirs('/root/data/model', exist_ok=True)
+    snapshot_download('Shanghai_AI_Laboratory/internlm-chat-7b', cache_dir='/root/data/model', revision='v1.0.3')
     os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
-
-    # 下载模型
     os.system(
         'huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir /root/data/model/sentence-transformer')
