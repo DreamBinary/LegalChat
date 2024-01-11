0b352e031cb2bdca5a1b994ef774aec2661af8c7
cxq <1647715359@qq.com>
2024-01-11 14:02:26 +0800

    update

--- download.py
+++ download.py
@@ -9,17 +9,18 @@ from modelscope import snapshot_download
 
 
 def download():
-    # create dir
+
+
+
+    os.system("git clone https://gitee.com/yzy0612/nltk_data.git  --branch gh-pages && "
+              "cd nltk_data && "
+              "mv packages/* ./ && "
+              "cd tokenizers && "
+              "unzip punkt.zip && "
+              "cd ../taggers && "
+              "unzip averaged_perceptron_tagger.zip")
     os.makedirs('data/model', exist_ok=True)
     snapshot_download('Shanghai_AI_Laboratory/internlm-chat-7b', cache_dir='data/model', revision='v1.0.3')
     os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
     os.system(
         'huggingface-cli download --resume-download sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2 --local-dir data/model/sentence-transformer')
-
-    os.system("git clone https://gitee.com/yzy0612/nltk_data.git  --branch gh-pages")
-    os.system("cd nltk_data")
-    os.system("mv packages/* ./")
-    os.system("cd tokenizers")
-    os.system("unzip punkt.zip")
-    os.system("cd ../taggers")
-    os.system("unzip averaged_perceptron_tagger.zip")
